a2dbf3a98ef2198802b0bb16ad2592eb1a7ceec8
Ansh Singh <144218874+AnshSingh2529@users.noreply.github.com>
2023-10-08 10:23:57 +0000

    datatypes summary done

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,21 @@
+// Primitive datatypes
+ 
+/*string , number , boolean , null , undefined , symbol , BigInt*/
+
+// Reference types (Non-primitive)
+
+/*Array , Objects , Function*/
+
+
+let heroes = ["Shaktimaan" , "Naagraj"]    // Arrays
+
+let myObjects = {                          // Objects
+    name:"ansh",
+    age:23
+}
+
+let myFunction = function(){                //Functions
+console.log();
+}
+
+
