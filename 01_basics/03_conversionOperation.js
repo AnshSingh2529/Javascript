25dedd34d193710debd8b16f5a2e806f3bc1fcc8
Ansh Singh <144218874+AnshSingh2529@users.noreply.github.com>
2023-10-08 08:04:35 +0000

    ConversionOperations done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33sa"
+
+// console.log(typeof score);
+
+let valueInNumber  = Number(score)  // We change the type but when we are going to print the value it prints (NaN) means Not a Number.
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+33 => 33
+"33abc" => NaN
+true => 1; false => 0;
+ */
+
+let isLoggedin  = 1
+let BooleanisLoggedin = Boolean(isLoggedin)
+// console.log(isLoggedin);
+
+/*
+1 => true 
+"" => false
+"ansh" => true
+ */
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+/*these types of conversion you should look carefully
+we will further learn while doing projects */
+
+
