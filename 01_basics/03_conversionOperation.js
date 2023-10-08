26e07983114bdd6a293058695a3c3450a8dbfe53
Ansh Singh <144218874+AnshSingh2529@users.noreply.github.com>
2023-10-08 09:49:06 +0000

    Comparisions done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,10 +25,42 @@ let BooleanisLoggedin = Boolean(isLoggedin)
 
 let somenumber = 33
 let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
 /*these types of conversion you should look carefully
 we will further learn while doing projects */
+
+
+/********Operations*******/
+
+let value = 3
+let negativeValue = -value
+console.log(negativeValue);
+
+// console.log(3+3);    addition
+// console.log(2-1);    substraction
+// console.log(2*3);    multiplication
+// console.log(2**3);   to the power
+// console.log(3/2);   division
+// console.log(9%3);   modulus
+
+let str1 = "hello"
+let str2 = "Shivam" 
+
+let str3 = str1 + str2
+
+// console.log(str3);
+// console.log("1" + 2);       // Output => 12
+// console.log(1 + "2");       // Output => 12
+// console.log("1" + 2 + 2);   // Output => 122
+// console.log(1 + 2 + "2");   //Output => 32  bcz numbers are first preference
+
+let getCounter = 100
+// getCounter++;       This is called postfix (Increments and return the value after Increamenting)
+// ++getCounter;       This is called prefix (Increments and return the value before Increamenting)
+console.log(getCounter);
+
+
 
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+     //Basics camparision
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 != 1);    
+
+
+// console.log("2" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);  // equality check
+// console.log(null < 0);
+// console.log(null >= 0);  // comparision converts null to a number.(==) & (< , >) both works differently
+
+/* ===  */   // datatype check
+console.log("2" === 2); 
+
+
+
