f1d41caa259687f27a6f9ecf6f8cbde37b35928e
Ansh Singh <144218874+AnshSingh2529@users.noreply.github.com>
2023-10-09 11:26:58 +0000

    strings done

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,32 @@
+// ----------------Strings-------------------
+
+const name = "Ansh"
+const repoCount = 50
+
+// console.log(name + repoCount + "good");      *This is a bad practice
+
+// console.log(`hello,my name is ${name} and my repocount is ${repoCount}`);    //*this is a good practice if (CONCATENATION)
+
+const gamename = new String('Anshsingh')
+// console.log(gamename);
+
+// -------Some Prototypes of String-------
+
+const groupname = gamename.substring(0 , 5)
+
+console.log(groupname);
+
+// More like that 
+
+// .tolowercase()
+// .touppercase()
+// ..split()
+// .slice()
+// .tostring()
+// .substring()
+// .replace()
+// .charAt()
+// .indexof()
+// .valueof()
+
+//many more like that visit (String javascript mdn at google.)
