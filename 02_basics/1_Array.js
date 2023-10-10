32ea8eaf4df8de94e9aa92649ff8deaa54b45b98
Ansh Singh <144218874+AnshSingh2529@users.noreply.github.com>
2023-10-10 12:06:50 +0000

    Array 01 Done

---
+++ 02_basics/1_Array.js
@@ -0,0 +1,62 @@
+                                    //Arrays               VISIT (Arrays javascript mdn)
+
+
+const myarr = [1,2,3,4,5,6]
+// console.log(myarr);
+
+// console.log(myarr[3]);   //If we want element at index[3].
+
+let newarr = new Array()   //We can use it as Object
+newarr.push("ansh","vansh","sachin")
+console.log(newarr);
+
+                                    // Array Methods
+
+
+// myarr.push(7)         //For adding new element in Array we use push().
+// console.log(myarr);
+
+// myarr.pop(5)             //Calculating from 0--length of the Array (Delete the element at that index)
+// console.log(myarr);
+
+
+// console.log(myarr.length);     //For length of the Array
+
+// shift and unshift
+
+// myarr.unshift(0)         //For adding element at the 0th index(IN front of the array)
+// console.log(myarr);
+
+// myarr.shift()            //For remove the element which is at the front (unshifted).
+// console.log(myarr);
+
+
+// console.log(newarr.indexOf("sachin"));   //It gives the index if the element
+// console.log(newarr.includes("sachin"));  //IT gives the boolean value that the element is present or not 
+
+
+// slice and splice
+
+/*
+
+console.log("A",myarr);
+const myn1 = myarr.slice(1,3)
+console.log(myn1);            //Return the value from (start to end ) according to parameters  [Note:it doesn't take the range value like, (3)]
+
+console.log("B",myarr);
+const myn2 = myarr.splice(1,3)
+console.log(myn2);              //It includes the range value 
+
+console.log("C",myarr);         // difference b/w slice and splice thats  splice manipulate the Array [Output:[1,5,6]] after splice.
+
+
+*/
+
+
+// In Array {--join()--} Function it join the wrap the array and its types string.
+
+
+
+
+
+
