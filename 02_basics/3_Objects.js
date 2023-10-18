1807edd3735c5f9f9b2114a35337c2e4b7aef4b6
Ansh Singh <144218874+AnshSingh2529@users.noreply.github.com>
2023-10-18 12:00:56 +0000

    3_objects done 2nd

--- 02_basics/3_Objects.js
+++ 02_basics/3_Objects.js
@@ -1,9 +1,6 @@
                 //   --------Objects part one---------      
 
-// Singleton--- (Object.create)
-
-
-// object literals
+// object literals type object creation 
 
 const mysym = Symbol("key1") 
 
@@ -42,19 +39,81 @@ const user = {
 
 // Function
 
-user.greetings = function(){
-    console.log("Hello, User");
-}
-// console.log(user.greetings);    //function ka refrence dega 
-console.log(user.greetings());    
+// user.greetings = function(){
+//     console.log("Hello, User");
+// }
+// // console.log(user.greetings);    //function ka refrence dega 
+// console.log(user.greetings());    
 
-user.greetingstwo = function(){
-    console.log(`Hello user,${this.name}`);
-}
+// user.greetingstwo = function(){
+//     console.log(`Hello user,${this.name}`);
+// }
 
-console.log(user.greetingstwo());
+// console.log(user.greetingstwo());
 
 //              --------Objects part Two---------
+
+
+//Singleton Object
+
+// const tinderUser = new Object()    // this is singleton type object creation 
+
+const tinderUser = {}
+
+tinderUser.userId = "abc_232"
+tinderUser.name = "Anuj"
+tinderUser.contact = 3702420842
+
+// console.log(tinderUser);     // Output is {}.
+
+const regularuser = {
+    email:"sanjguufg@CharacterData.com",
+    fullname: {
+        firstname:"anj",
+        lastname:"girja"
+    }
+}
+// console.log(regularuser.fullname);
+
+const obj1 = {1:"a" , 2:"b"}
+const obj2= {3:"b" , 4:"d"}
+
+// const obj3 = Object.assign({},obj1,obj2)      //.assign()   help in combining the values
+const obj3 = {...obj1,...obj2}    //spread concept
+
+
+// console.log(obj3);
+
+const USER = [                       //it is called as Arrays of Objects
+    {
+        email:"a@gmail.com",
+        name:"anuj"
+},
+    {
+        1:"a",
+        2:"b"
+},
+    {
+        1:"a",
+        2:"b"
+}
+]
+
+// console.log(USER[0]);      //for accessing single values fromn Arrays of Objects
+
+// console.log(tinderUser);
+
+//some prototypes of objects
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("name"));
+
+
+
+
+
 
 
 
