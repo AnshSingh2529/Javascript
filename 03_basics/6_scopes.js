bf13b8e03fbc11e3f0870e93bd711a6bd27b2a19
Ansh Singh <anshsingh2529@gmail.com>
2023-10-20 08:43:41 +0000

    6_scopes done

--- 03_basics/6_scopes.js
+++ 03_basics/6_scopes.js
@@ -8,8 +8,8 @@ let a = 300;     //Global scope -> jo values ham block ke bahar hi assign aur pr
 if(true){       //Local scope -> jo ham block ke ander assign krke print karate hain
     let a = 10;
     const b = 30;
-    console.log(a);     //returns the values assign in local scope
-    console.log(b);     //returns the values assign in local scope
+    // console.log(a);     //returns the values assign in local scope
+    // console.log(b);     //returns the values assign in local scope
     
 }
 
@@ -21,4 +21,45 @@ if(true){       //Local scope -> jo ham block ke ander assign krke print karate 
 
 //---websites pr inspect krke (console) me (global scope) different hota hai aur (node) use krke terminal se krna different hota hai.
 
+
+//---------Nested function---------
+
+function one(){
+    const username = "ansh"
+
+    function two(){
+        const websites = "youtube"
+        // console.log(username);
+        // console.log(websites);      //it Prints here bcz defined in same block.
+    }
+    // console.log(websites);      //it gives an error ...website is undefined.
+    two()
+}
+one()
+
+
+if(true){
+    const name = "ansh"
+    if(name === "ansh"){
+        const website = " youtube"
+        // console.log(name + website);
+    }
+    // console.log(website);         //same condition gives an error that website is undefined
+
+}
+// console.log(name);         //it also gives an error bcz used in outside loop
+
+
+//++++++++++++++++ Interesting ++++++++++++++
+
+console.log(addone(5));                 //we can write here before initialise the function
+function addone(num){
+return num+1
+}
+
+// addtwo(4)                  //ReferenceError: Cannot access 'addtwo' before initialization
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(4));
 
