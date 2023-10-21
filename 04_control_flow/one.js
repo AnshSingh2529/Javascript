87199b72d5442049eab730566f6cc712681267f4
Ansh Singh <anshsingh2529@gmail.com>
2023-10-21 10:26:24 +0000

    not done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+//if
+
+// if (true){                     //In the parathesis of (if) condition should be always true.
+
+// }
+
+//condtions are --> < , > , <= , >= , !=, == , !== , ===(this is specially for type check).for example
+
+//1.
+
+// if(2 == 2){
+//     console.log(`executed`);
+// }
+
+// if (2==="2"){                     //type checking thats why not executable
+//     console.log(`executed`);     
+// }
+
+//etc.......
+
+// const temperature = 55
+
+// if (temperature < 50){
+//     console.log(`normal temp`);
+// } else{
+//     console.log(`danger! high temp`);
+// }
+
+// console.log(`code executed`);       //it is always execute bcz it is outside my if and else statement.
+
+//sample code----------------------------
+
+// const score = 400
+
+// if (score > 90){
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+// } else{
+//     const power = "walk"
+//     console.log(`user power : ${power}`);
+// }
+
+//----------------------------------------
+
+// const marks = 100
+// if (marks > 50) console.log("pass"); //it will execute without scope (implicit scope) but not a good practice.
+
+
+
+
+
+
+
+
