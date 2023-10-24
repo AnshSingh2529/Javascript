4a30a1c0cf4e70734406e30037726d0278fd467a
Ansh Singh <anshsingh2529@gmail.com>
2023-10-24 09:40:57 +0000

    for loop and break & continue

---
+++ 05_iterators/one.js
@@ -0,0 +1,45 @@
+// --------for loop----------
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log(" 5 is here");
+    }
+    // console.log(element);
+
+}
+
+for (let i = 1; i <= 10; i++) {
+//    console.log(`Table of:${i}`);
+    for (let j = 1; j <= 10; j++) {
+    //    console.log(`${i} * ${j} = ${i * j}`);
+        
+    }
+}
+
+const myarray = ["superman", "shaktimaan" , "ironman"]
+
+for(let  i = 0; i< myarray.length; i++){
+    const element = myarray[i];
+    // console.log(element);
+}
+
+//continue and break
+
+
+// for(let i = 1; i<=20; i++){    i
+//     if(i == 5){
+//         console.log(`detected 5`);
+//         break
+//     }
+//     console.log(` index: ${i}`);
+// }
+
+
+// for(let i = 1; i<=20; i++){    i
+//     if(i == 5){
+//         console.log(`detected 5`);
+//         continue
+//     }
+//     console.log(` index: ${i}`);
+// }
